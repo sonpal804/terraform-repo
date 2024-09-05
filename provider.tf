73709650db5341a670ada2f483ea9c2bbdfacb28
sonpal804 <sonujadon225@gmail.com>
2024-09-05 13:56:53 +0530

    Update provider.tf......

--- provider.tf
+++ provider.tf
@@ -11,8 +11,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
-  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = "clientid"
+  client_secret   = "clientsecret"
+  tenant_id       = "tenantid"
+  subscription_id = "subscriptionid"
 }
