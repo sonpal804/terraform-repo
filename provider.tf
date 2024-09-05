d9422266dc84f4da73ad2fbcfc97221776e45eda
jadon <sonujadon225@gmail.com>
2024-09-05 13:18:16 +0530

    files are modified

--- provider.tf
+++ provider.tf
@@ -11,8 +11,8 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  client_id       = "clientid"
-  client_secret   = "clientsecret"
-  tenant_id       = "tenantid"
-  subscription_id = "subscriptionid"
+  client_id       = env.ARM_CLIENT_ID
+  client_secret   = env.ARM_CLIENT_SECRET
+  tenant_id       = env.ARM_TENANT_ID
+  subscription_id = env.ARM_SUBSCRIPTION_ID
 }
