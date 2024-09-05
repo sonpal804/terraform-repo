186f8b378051fd91c89274610d08f5420adfaaf6
sonpal804 <sonujadon225@gmail.com>
2024-09-05 13:59:00 +0530

    Update and rename backend.tf1 to backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,10 @@ terraform {
     resource_group_name  = "sonpal-vm_group"  
     storage_account_name = "terraformbackendsonu"                
     container_name       = "tfstate"                       
-    key                  = "terraform.tfstate"       
+    key                  = "terraform.tfstate"  
+    client_id       = "clientid"
+    client_secret   = "clientsecret"
+    tenant_id       = "tenantid"
+    subscription_id = "subscriptionid"
   }
 }
