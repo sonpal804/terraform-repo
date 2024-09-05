7cf9a93d57c4916addb9c2105c47348672cc1471
jadon <sonujadon225@gmail.com>
2024-09-05 11:25:17 +0530

    provider is updated

--- provider.tf
+++ provider.tf
@@ -17,5 +17,13 @@ provider "azurerm" {
   client_id       = var.azure_client_id
   client_secret   = var.azure_client_secret
   
+}
+
+provider "azuread" {
+  features {}
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret 
 
 }
