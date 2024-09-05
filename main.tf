d1011767f0ed557aff0dab6b2813d36da37ad64b
sonpal804 <sonujadon225@gmail.com>
2024-09-05 14:03:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rgv1" {
-  name     = "myTFResourceGroupv1"
+resource "azurerm_resource_group" "sonpal-vm_group" {
+  name     = "sonpal-vm_group_rg1"
   location = "westus2"
 }
